78b7521e6b7d999053d1d0aead577cc66e079e90
thanhvg <thanhvg@gmail.com>
2016-05-20 19:22:20 -0600

    update

--- functions.php
+++ functions.php
@@ -76,6 +76,37 @@ function addAgent($agent)
 
 }
 
+function addAgentI($agent)
+{
+  $values = array_values($agent);
+  //connect to db
+  $myconn = connectdb();
+
+  //transfer data to sql string
+
+  $sql = "INSERT INTO `agents` (`AgtFirstName`, `AgtMiddleInitial`, `AgtLastName`, `AgtBusPhone`, `AgtEmail`, `AgtPosition`, `AgencyId`) VALUES (?, ?, ?, ?, ?, ?, ?);";
+  $stmt = mysqli_prepare($myconn, $sql);
+  mysqli_stmt_bind_param($stmt, "sssissi", $values[0], $values[1], $values[2], $values[3], $values[4], $values[5], $values[6]);
+
+  //send query to db
+  $result = mysqli_stmt_execute($stmt);
+
+  //close connection
+  mysqli_close($myconn);
+
+  //check results
+  if ($result)  {
+    $msg ="SUCCESSED: ".$sql;
+    writelog("log.txt",$msg);
+    return true;
+  }  else  {
+    $msg ="FAILED: ".$sql;
+    writelog($file,$msg);
+    return false;
+  }
+
+}
+
 function writelog($file, $msg) {
   $fh = fopen($file, "a");
   $msg = date("F j, Y, g:i a")." ".$msg."\n";
